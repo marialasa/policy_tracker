16cf7a853ca4eabb2f3994b6a461563662c1ed1d
marialasa <62130723+marialasa@users.noreply.github.com>
2020-04-24 01:28:02 -0300

    Update tracker.R

--- script/tracker.R
+++ script/tracker.R
@@ -1,25 +1,25 @@
 # Instalar libraries
+library(readr)
 library(ggplot2)
 library(gganimate)
 
 # Cargar datasets
-tracker <- read.csv("file")
-attach(file)
+dataset <- read_csv("dataset.csv")
 
 # Crear plot. Ajustar valores en rows 17, 21 & 22.
 # Pueden personalizarse también los códigos de colores de row 25.
 p <- ggplot(
-    Tracker,
+    dataset,
     aes(Dia, No_Politicas, color = factor(Tipo))
 ) +
     theme_minimal() +
     geom_line(size = 1) +
-    geom_segment(aes(xend = 48, yend = No_Politicas, group = Tipo),
+    geom_segment(aes(xend = 50, yend = No_Politicas, group = Tipo),
                  linetype = 0, colour = 'white') +
     geom_text(aes(x = XX, label = Tipo, size = 5), hjust = 0) +
     guides(color = FALSE) +
-    scale_x_continuous(breaks = seq(0, 48, by = 5),
-                       limits = c(0, 62))+
+    scale_x_continuous(breaks = seq(0, 50, by = 5),
+                       limits = c(0, 65))+
     xlab("")+
     ylab("")+
     scale_color_manual(values=c('#E17C05', '#cac9cb', '#edad08', '#cc503e', '#94346e', '#1d6996', '#73af48', '#000000', '#00ade6')) +
@@ -30,7 +30,6 @@ p <- ggplot(
     theme(legend.position = "none")
 
 # Run the plot
-
 p
 
 # Crear animación
