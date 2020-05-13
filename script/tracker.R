51c101bf7697849d590800934a76f70d07a8c3ff
marialasa <62130723+marialasa@users.noreply.github.com>
2020-05-13 11:10:59 -0300

    Update tracker.R

--- script/tracker.R
+++ script/tracker.R
@@ -4,37 +4,53 @@ library(ggplot2)
 library(gganimate)
 
 # Cargar dataset
-dataset <- read_delim("dataset.csv", ";", 
-                      escape_double = FALSE,
-                      trim_ws = TRUE)
+dataset <- read.table(file = "https://github.com/marialasa/policy_tracker/blob/master/dataset/dataset.csv",
+                           sep=';',
+                           header = TRUE,
+                           stringsAsFactors = FALSE)
 
-# Crear plot. Ajustar valores en rows 19, 21, 23 & 24.
-# Pueden personalizarse también los códigos de colores de row 27.
-p <- ggplot(
-  dataset,
-  aes(Dia, No_Politicas, color = factor(Tipo))
-) +
-  theme_test() +
-  geom_line(size = 0.5) +
-  geom_segment(aes(xend = 63, yend = No_Politicas, group = Tipo),
-               linetype = "dashed", size = 0.3, colour = "black") +
-  geom_text(size = 3.5, aes(x = 63, label = Tipo, size = 5), hjust = 0) +
+# Diseño de plot. Ajustar valores en rows 18:28.
+# Pueden personalizarse también los códigos de colores en rows 33:41.
+p <- ggplot(dataset,
+            aes(Dia, No_Politicas, color = factor(Tipo))) +
+  theme_classic() +
+  geom_line(size = 1.5) +
+  geom_segment(
+    aes(xend = 69, yend = No_Politicas, group = Tipo),
+    linetype = "dashed",
+    size = 0.5,
+    colour = "black"
+  ) +
+  geom_text(size = 6,
+            aes(x = 69, label = Tipo, size = 5),
+            hjust = 0) +
   guides(color = FALSE) +
-  scale_x_continuous(breaks = seq(0, 63, by = 5),
-                     limits = c(0, 77))+
-  xlab("")+
-  ylab("")+
-  scale_color_manual(values=c('#E17C05', '#cac9cb', '#edad08', '#cc503e', '#94346e', '#1d6996', '#73af48', '#000000', '#00ade6')) +
+  scale_x_continuous(breaks = seq(0, 70, by = 5),
+                     limits = c(0, 80)) +
+  xlab("") +
+  ylab("") +
+  scale_color_manual(
+    values = c(
+      '#E17C05',
+      '#cac9cb',
+      '#edad08',
+      '#cc503e',
+      '#94346e',
+      '#1d6996',
+      '#73af48',
+      '#000000',
+      '#00ade6'
+    )
+  ) +
   coord_cartesian(clip = 'off') +
-  ggtitle("Policy Tracker: respuestas a Covid-19 en Argentina", "Plot de http://medium.com/@Condolasa"
-) +
+  ggtitle(
+    "Policy Tracker: respuestas a Covid-19 en Argentina",
+    "Plot de http://medium.com/@Condolasa"
+  ) +
   labs(x = "Días desde primer infectado", y = "Número de políticas implementadas", caption = "Data: https://www.argentina.gob.ar/coronavirus/medidas-gobierno") +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  geom_point(size = 3) +
+  transition_reveal(Dia)
 
-# Run the plot
-p
-
-# Crear animación
-p + 
-    geom_point(size = 2.5) +
-    transition_reveal(Dia)
+# Ajustes
+animate(p, width = 2100, height = 1500, res = 150)
