4a02e0e20e0879f6721dff222a0edb85c1c779e4
marialasa <62130723+marialasa@users.noreply.github.com>
2020-05-23 22:07:25 -0300

    Update tracker.R

--- script/tracker.R
+++ script/tracker.R
@@ -14,33 +14,24 @@ dataset <- read.table(file = "https://github.com/marialasa/policy_tracker/blob/m
 p <- ggplot(dataset,
             aes(Dia, No_Politicas, color = factor(Tipo))) +
   theme_classic() +
-  geom_line(size = 1.5) +
+  geom_line(size = 1) +
   geom_segment(
-    aes(xend = 69, yend = No_Politicas, group = Tipo),
+    aes(xend = 80, yend = No_Politicas, group = Tipo),
     linetype = "dashed",
     size = 0.5,
     colour = "black"
   ) +
-  geom_text(size = 6,
-            aes(x = 69, label = Tipo, size = 5),
+  geom_text(size = 5,
+            aes(x = 80, label = Tipo, size = 5),
             hjust = 0) +
   guides(color = FALSE) +
-  scale_x_continuous(breaks = seq(0, 70, by = 5),
-                     limits = c(0, 80)) +
+  scale_x_continuous(breaks = seq(0, 81, by = 5),
+                     limits = c(0, 90)) +
   xlab("") +
   ylab("") +
   scale_color_manual(
-    values = c(
-      '#E17C05',
-      '#cac9cb',
-      '#edad08',
-      '#cc503e',
-      '#94346e',
-      '#1d6996',
-      '#73af48',
-      '#000000',
-      '#00ade6'
-    )
+    values = c('#E17C05', '#cac9cb', '#edad08', '#cc503e',
+               '#94346e', '#1d6996', '#73af48', '#000000', '#00ade6')
   ) +
   coord_cartesian(clip = 'off') +
   ggtitle(
@@ -49,8 +40,8 @@ p <- ggplot(dataset,
   ) +
   labs(x = "Días desde primer infectado", y = "Número de políticas implementadas", caption = "Data: https://www.argentina.gob.ar/coronavirus/medidas-gobierno") +
   theme(legend.position = "none") +
-  geom_point(size = 3) +
+  geom_point(size = 2.5) +
   transition_reveal(Dia)
 
-# Ajustes
-animate(p, width = 2100, height = 1500, res = 150)
+# Guardar plot
+animate(p, width = 1280, height = 720, res = 72)
